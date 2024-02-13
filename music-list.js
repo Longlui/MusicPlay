38b6286103fbda22b1bd52369dbaf31ba6f19b33
Long <longat1901@gmail.com>
2024-02-13 08:32:35 +0700

    tet MusicPlay project

---
+++ music-list.js
@@ -0,0 +1,59 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "Không Biết Phải Làm Sao",
+    artist: "Dương Lan Nhi",
+    img:"KhongBietPhaiLamSao",
+    src:"KhongBietPhaiLamSao"
+},
+{
+    name: "Không sao em à",
+    artist: "Huy",
+    img:"KKhongSaoEmA",
+    src:"KhongSaoEmA"
+},
+{
+    name: "Sao em vô tình",
+    artist: "Jack",
+    img:"SaoEmVoTinh",
+    src:"SaoEmVoTinh"
+},
+{
+    name: "Mưa rơi vào phòng",
+    artist: "Khởi My",
+    img:"MuaRoiVaoPhong",
+    src:"MuaRoiVaoPhong"
+},
+{
+    name: "Em ơi anh phải làm sao",
+    artist: "Dương Minh Tuấn",
+    img:"EmOiAnhPhaiLamSao",
+    src:"EmOiAnhPhaiLamSao"
+},
+{
+    name: "Bạc Phận",
+    artist: "Jack",
+    img:"BacPhan",
+    src:"BacPhan"
+},
+{
+    name: "Làm người luôn yêu em",
+    artist: "Sơn Tùng M-TP",
+    img:"LamNguoiLuonYeuEm",
+    src:"LamNguoiLuonYeuEm"
+},
+{
+    name: "Phía sau em",
+    artist: "KayTran & Bin-z",
+    img:"PhiaSauEm",
+    src:"PhiaSauEM"
+},
+];
